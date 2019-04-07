167e5f0ceacfd59951b02aed287739fb58fbedf0
Michael <micbayne@bu.edu>
2019-04-06 20:19:30 -0400

    HOTFIX

--- ProjectCode/database.js
+++ ProjectCode/database.js
@@ -1,8 +1,45 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/mydb";
 
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    console.log("Database created!");
-    db.close();
-});
+function createCollection(collectionName) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.createCollection(collectionName, function(err, res) {
+            if (err) throw err;
+            db.close();
+        });
+    });
+}
+
+function insertIntoCollection(collection, obj) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.collection(collection).insertOne(obj, function(err, res) {
+            if (err) throw err;
+            db.close();
+        });
+    });
+}
+
+function findAll(collection) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.collection(collection).find({}).toArray(function(err, result) {
+            if (err) throw err;
+            db.close();
+            console.log(result);
+        });
+    });
+}
+
+
+createCollection("RoadTrips");
+var obj = {
+    'number': 1,
+    'number2': 2
+};
+insertIntoCollection("RoadTrips", obj);
+console.log(findAll('RoadTrips'));
